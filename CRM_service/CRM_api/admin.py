e45c27cdb060c0553a43ab27ca42c6ef2e323de8
Maksim <106476320+MaksimZelinskiy@users.noreply.github.com>
2022-06-30 15:41:09 +0300

    [upd] commit админка, модель заявки

---
+++ CRM_service/CRM_api/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Order, Device, Customer, DeviceInField
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'manufacturer', 'model')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device', 'customers', 'order_description', 'created_at', 'last_updated_at', 'order_status')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'customer_address', 'customer_city')
+
+class DeviceInFieldAdmin(admin.ModelAdmin):
+    list_display = ('id', 'serial_number', 'customers_id', 'analyzer_id', 'owner_status')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(DeviceInField, DeviceInFieldAdmin)
+
+
